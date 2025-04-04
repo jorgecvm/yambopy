6944c5bb211ad5d956357d0c92e5f98fddc62fd5
jorgecvm <cervantes.jorge9@gmail.com>
2025-04-04 15:58:31 +0200

    Creating rt_carriers class to plot carriers on BZ

--- yambopy/rt/rt_carriers.py
+++ yambopy/rt/rt_carriers.py
@@ -18,30 +18,42 @@ class YamboRTcarriers():
 
        RT_kpt, RT_carriers_delta_f = read_netcdf(carriers_path)
 
+       self.RT_kpoints = RT_kpt.T
+
        # Initialize the new array with dimensions (time, 144)
+       self.RT_kpt = RT_kpt
        self.Time = RT_carriers_delta_f.shape[0]
        self.Kpt = RT_kpt.shape[1]
        self.nbands = int(RT_carriers_delta_f.shape[1]/self.Kpt)
 
-       Carriers = np.zeros((self.Time, self.Kpt))
+       self.Carriers = np.zeros((self.Time, self.Kpt))
 
        for t in range(self.Time):
            # Select the third value from every group of 4 along the RTstates dimension
            for ik in range(self.Kpt):
-               Carriers[t, ik] = RT_carriers_delta_f[t, ik*self.nbands + sel_band]  # Selects the third value (index 2 of every group)
+               self.Carriers[t, ik] = RT_carriers_delta_f[t, ik*self.nbands + sel_band] 
 
-       print(RT_kpt.T)
 
-       exit()
+    def plot_carriers_ax(self,ax,file_path):
 
-    def plot_carriers_ax(file_path):
- 
-        for t in range(RT_carriers_delta_f.shape[0]-1):
-            # Select the third value from every group of 4 along the RTstates dimension
-            for i in range(144):
-                New_array[t, i] = RT_carriers_delta_f[t, i*X + selected_band]  # Selects the third value (index 2 of every group)
-        print(New_array[399,:])
+        prueba = YamboLatticeDB.from_db_file(filename='SAVE/ns.db1')
 
+        Kpints_bien = prueba.red_kpoints
+        self.rlat = prueba.rlat
+
+        kmesh_full, kmesh_idx = replicate_red_kmesh(Kpints_bien,repx=range(-1,2),repy=range(-1,2))
+        x,y = red_car(kmesh_full,self.rlat)[:,:2].T
+
+        self.Carriers[:,:] = self.Carriers[:,kmesh_idx]
+       
+        ax.scatter(x,y)
+  
+
+        '''
+        for ik in range(self.Kpt): 
+            ax.scatter(Kpints_bien[ik,0],Kpints_bien[ik,1], c = self.Carriers[399,ik], vmin = -2.0e-4, vmax = 2.0e-4, cmap = 'viridis', s = 30)
+            print(self.Carriers[399,ik])
+        '''
 
 def read_netcdf(carriers_path):
     with nc.Dataset(carriers_path, 'r') as dataset:
